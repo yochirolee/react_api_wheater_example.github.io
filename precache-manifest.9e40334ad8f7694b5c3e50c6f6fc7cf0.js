8c11c7247568105a0e83521357eceba884d36c8b
yochirolee <yleecruz@gmail.com>
2020-05-10 05:49:05 -0500

    Updates

---
+++ precache-manifest.9e40334ad8f7694b5c3e50c6f6fc7cf0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ced9df003b09394ddf305717dfdcaa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ef3d27603e71a3ceacd",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4a836411b47b5ef45aba",
+    "url": "/static/css/main.fbd9d2cf.chunk.css"
+  },
+  {
+    "revision": "6ef3d27603e71a3ceacd",
+    "url": "/static/js/2.27f9b574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27f9b574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a836411b47b5ef45aba",
+    "url": "/static/js/main.b40b924c.chunk.js"
+  },
+  {
+    "revision": "6b31fe6424540ab611f9",
+    "url": "/static/js/runtime-main.e095632a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
